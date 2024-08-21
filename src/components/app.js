5e54183fe29bd850134b56faf592303aa5feab3a
jcamerom <juan.camero.m@gmail.com>
2024-08-21 17:52:56 +0200

    Primeras modificaciones de la plantilla

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import moment from "moment";
 
 export default class App extends Component {
   render() {
@@ -6,6 +7,9 @@ export default class App extends Component {
       <div className='app'>
         <h1>DevCamp React Starter</h1>
         <h2>React Redux Router</h2>
+        <div>
+          {moment().format('MMMM Do YYYY, h:mm:ss a')}
+        </div>
       </div>
     );
   }
